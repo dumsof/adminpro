426d7d66454cd33ffd7c58b8e394ea8c82c92052
Darwin <darwinu34@yahoo.es>
2020-05-30 16:46:53 -0500

    se crea el loguin y recuerdame.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '@/services/service.index';
+import Swal from 'sweetalert2';
 
 /* DUM: se llama la función del archivo de js/custom.js para que se cargue el menu dinamico sidebar */
 declare function inicio_plugins();
@@ -10,17 +13,29 @@ declare function inicio_plugins();
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(public ruoter: Router) { }
+  correo = '';
+  recuerdame = false;
+  constructor(public ruoter: Router, private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
     inicio_plugins();
+    this.correo = localStorage.getItem('email') || '';
+    if (this.correo.length > 0) {
+      this.recuerdame = true;
+    }
+
   }
 
-  ingresar() {
-    console.log('ingresando');
-    this.ruoter.navigate(['/dashboard']);
+  ingresar(formulario: NgForm) {
+    if (!formulario.valid) {
+      return;
+    }
 
+    console.log(formulario.valid, formulario.value.email);
+
+    this.usuarioService.login(formulario.value, formulario.value.recuerdame)
+      .subscribe(respuesta => this.ruoter.navigate(['/dashboard'])
+        , error => Swal.fire('Usuario no Éxiste', `El usuario o contraseña son invalidos, por favor verifique`, 'info'));
   }
 
 }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -15,6 +15,25 @@ export class UsuarioService {
 
   constructor(public http: HttpClient) { }
 
+  login(usuario: Usuario, recordar: boolean = false) {
+    const usuarioLogueo = {
+      email: usuario.correo,
+      password: usuario.password
+    };
+    if (recordar) {
+      localStorage.setItem('email', usuario.correo);
+    } else {
+      localStorage.removeItem('email');
+    }
+    const url = `${environment.URL_SERVICIOS}/login`;
+    return this.http.post(url, usuarioLogueo).pipe(map((respuesta: any) => {
+      localStorage.setItem('id', respuesta.id);
+      localStorage.setItem('token', respuesta.id);
+      localStorage.setItem('usuario', JSON.stringify(respuesta.usuario));
+      return respuesta.ok;
+    }));
+  }
+
   crearUsuario(usuario: Usuario) {
     const datosUsuario = {
       nombre: usuario.nombre,
@@ -24,11 +43,10 @@ export class UsuarioService {
     };
     const url = `${environment.URL_SERVICIOS}/usuario`;
     return this.http.post(url, datosUsuario).pipe(map((respuesta: any) => {
-      Swal.fire('Usuario Creado', `Usuario ${ usuario.correo} creado con éxito.`, 'success');
+      Swal.fire('Usuario Creado', `Usuario ${usuario.correo} creado con éxito.`, 'success');
       return respuesta.usuario;
     }));
-
-
   }
+
 
 }
